436a656c13a8bc9237da244e4f3ecfb3017edf3f
charl <charl@DESKTOP-A4DK2O9>
2022-01-21 09:00:30 +0000

    added fetch to store/loadTodos

--- src/stores/TodoStore.js
+++ src/stores/TodoStore.js
@@ -1,20 +1,29 @@
 import {writable} from 'svelte/store';
 
+const url = 'http://127.0.0.1:8000/';
 
-export const todos = writable(
+export let todos = writable(
     [
-		{ 
-			id: 1, 
-			text: 'Todo1',
-			completed: false
-		},
-		{ 
-			id: 2, 
-			text: 'Todo2', 
-			completed: true
-		}
+		// { 
+		// 	id: 1, 
+		// 	text: 'Todo1',
+		// 	completed: false
+		// },
+		// { 
+		// 	id: 2, 
+		// 	text: 'Todo2', 
+		// 	completed: true
+		// }
 	]
 );
+
+export const loadTodos = async () => {
+	fetch(url)
+		.then(res => res.json())
+		.then(data => todos.set(data));
+}
+
+loadTodos();
 
 
 export const createTodo = (newTodo) => {
